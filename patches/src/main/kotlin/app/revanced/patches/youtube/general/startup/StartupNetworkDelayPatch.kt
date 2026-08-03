795382f8cb49b616cb80aac8cc32ec529aa27dcc
Nguyễn Tấn Vĩnh <potter.harry0909@yahoo.com>
2026-08-03 21:29:51 +0700

    test

--- patches/src/main/kotlin/app/revanced/patches/youtube/general/startup/StartupNetworkDelayPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/youtube/general/startup/StartupNetworkDelayPatch.kt
@@ -3,8 +3,11 @@ package app.revanced.patches.youtube.general.startup
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.patch.bytecodePatch
 import app.revanced.patches.youtube.utils.compatibility.Constants.COMPATIBLE_PACKAGE
+import app.revanced.patches.youtube.utils.extension.Constants.UTILS_PATH
 import app.revanced.patches.youtube.utils.patch.PatchList.STARTUP_NETWORK_DELAYED
 import app.revanced.util.fingerprint.methodOrThrow
+import app.revanced.util.indexOfFirstInstruction
+import app.revanced.util.indexOfFirstInstructionOrThrow
 import com.android.tools.smali.dexlib2.Opcode
 
 private const val EXTENSION_CLASS_DESCRIPTOR = "$UTILS_PATH/StartupNetworkDelayPatch;"
@@ -29,15 +32,7 @@ val delayStartupNetworkPatch = bytecodePatch(
 
         // 2. Hook vào hàm kiểm tra mạng của YouTube
         networkInfoFingerprint.methodOrThrow().apply {
-            // Lấy danh sách các dòng lệnh smali của hàm
-            val instructions = implementation!!.instructions
-            
-            // Tìm vị trí dòng lệnh trả về kết quả (MOVE_RESULT_OBJECT)
-            val resultIndex = instructions.indexOfFirst { it.opcode == Opcode.MOVE_RESULT_OBJECT }
-            
-            // Báo lỗi nếu không tìm thấy (để tránh build lỗi ngầm)
-            if (resultIndex == -1) error("Không tìm thấy lệnh MOVE_RESULT_OBJECT trong hàm kiểm tra mạng")
-
+            val resultIndex = indexOfFirstInstructionOrThrow(Opcode.MOVE_RESULT_OBJECT)
             addInstructions(
                 resultIndex + 1,
                 """
