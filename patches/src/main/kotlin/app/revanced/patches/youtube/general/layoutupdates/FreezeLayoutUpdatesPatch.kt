eadca474283883c09cf0b354dbfc086de9bb4ee7
Nguyễn Tấn Vĩnh <potter.harry0909@yahoo.com>
2026-08-04 21:38:01 +0700

    test

--- patches/src/main/kotlin/app/revanced/patches/youtube/general/layoutupdates/FreezeLayoutUpdatesPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/youtube/general/layoutupdates/FreezeLayoutUpdatesPatch.kt
@@ -8,11 +8,31 @@ import app.revanced.patches.youtube.utils.patch.PatchList.FREEZE_LAYOUT_UPDATES
 import app.revanced.util.fingerprint.matchOrThrow
 import app.revanced.util.indexOfFirstInstructionOrThrow
 import com.android.tools.smali.dexlib2.Opcode
+import com.android.tools.smali.dexlib2.iface.instruction.FiveRegisterInstruction
+import com.android.tools.smali.dexlib2.iface.instruction.Instruction
 import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
 import com.android.tools.smali.dexlib2.iface.instruction.ReferenceInstruction
+import com.android.tools.smali.dexlib2.iface.instruction.RegisterRangeInstruction
+import com.android.tools.smali.dexlib2.iface.instruction.ThreeRegisterInstruction
+import com.android.tools.smali.dexlib2.iface.instruction.TwoRegisterInstruction
 import com.android.tools.smali.dexlib2.iface.reference.MethodReference
 
 private const val EXTENSION_CLASS_DESCRIPTOR = "$UTILS_PATH/FreezeLayoutUpdatesPatch;"
+
+/**
+ * Hàm hỗ trợ lấy chính xác tên thanh ghi đích (VD: "v1", "v3", "v4") từ bất kỳ loại lệnh Smali nào.
+ */
+private fun Instruction.getRegister(): String {
+    val reg = when (this) {
+        is OneRegisterInstruction -> registerA
+        is TwoRegisterInstruction -> registerA
+        is ThreeRegisterInstruction -> registerA
+        is FiveRegisterInstruction -> registerC
+        is RegisterRangeInstruction -> startRegister
+        else -> error("Lệnh ${opcode.name} không hỗ trợ trích xuất thanh ghi A.")
+    }
+    return "v$reg"
+}
 
 @Suppress("unused")
 val freezeLayoutUpdatesPatch = bytecodePatch(
@@ -24,18 +44,14 @@ val freezeLayoutUpdatesPatch = bytecodePatch(
     execute {
         hotConfigPreferenceFingerprint.matchOrThrow().let { match ->
             match.method.apply {
-                // Lấy danh sách toàn bộ các lệnh (instructions) trong method
-                val instructions = implementation!!.instructions.toList()
-                
                 // --- 1. HOT CONFIG GROUP ---
                 val hotStoredTimestampStrIndex = match.stringMatches!!.first().index
                 
-                // Tìm ngược từ vị trí chuỗi "hot_stored_timestamp", lấy lệnh move-result-object gần nhất (chính là getString của hot_config)
-                val hotConfigGroupResultIndex = instructions.subList(0, hotStoredTimestampStrIndex)
+                // Tìm ngược từ vị trí "hot_stored_timestamp", lấy lệnh move-result-object gần nhất
+                val hotConfigGroupResultIndex = implementation!!.instructions.take(hotStoredTimestampStrIndex)
                     .indexOfLast { it.opcode == Opcode.MOVE_RESULT_OBJECT }
                 
-                // Đọc thanh ghi đích động (VD: nó sẽ tự động ra v1, v4... tùy theo smali)
-                val hotConfigReg = "v" + (instructions[hotConfigGroupResultIndex] as OneRegisterInstruction).registerA
+                val hotConfigReg = implementation!!.instructions.elementAt(hotConfigGroupResultIndex).getRegister()
 
                 addInstructions(
                     hotConfigGroupResultIndex + 1,
@@ -48,38 +64,38 @@ val freezeLayoutUpdatesPatch = bytecodePatch(
                 // --- 2. HOT HASH DATA ---
                 val hotStoredTimestampResultIndex = indexOfFirstInstructionOrThrow(hotConfigGroupResultIndex, Opcode.MOVE_RESULT_WIDE)
                 val hotHashDataInvokeIndex = indexOfFirstInstructionOrThrow(hotStoredTimestampResultIndex, Opcode.INVOKE_INTERFACE)
-                // Thay vì +1, ta tìm chính xác lệnh MOVE_RESULT_OBJECT để lấy thanh ghi động
                 val hotHashDataResultIndex = indexOfFirstInstructionOrThrow(hotHashDataInvokeIndex, Opcode.MOVE_RESULT_OBJECT)
                 
-                val hotHashReg = "v" + (instructions[hotHashDataResultIndex] as OneRegisterInstruction).registerA
+                val hotHashReg = implementation!!.instructions.elementAt(hotHashDataResultIndex).getRegister()
 
-                addInstructions(hotHashDataResultIndex + 1,
+                addInstructions(
+                    hotHashDataResultIndex + 1,
                     """
                         invoke-static {$hotHashReg}, $EXTENSION_CLASS_DESCRIPTOR->getHotHashData(Ljava/lang/String;)Ljava/lang/String;
                         move-result-object $hotHashReg
-                    """)
+                    """
+                )
             }
         }
 
         coldConfigPreferenceFingerprint.matchOrThrow().let { match ->
             match.method.apply {
-                val instructions = implementation!!.instructions.toList()
-
                 // --- 3. COLD HASH DATA ---
                 val coldHashDataStrIndex = match.stringMatches!![2].index
                 
-                // Tìm ngược từ vị trí "cold_hash_data", lấy lệnh iget-object gần nhất (chính là lệnh gán data vào v3 trong smali của bạn)
-                val coldHashIgetObjectIndex = instructions.subList(0, coldHashDataStrIndex)
+                // Lấy lệnh iget-object gần nhất phía trước chuỗi cold_hash_data
+                val coldHashIgetObjectIndex = implementation!!.instructions.take(coldHashDataStrIndex)
                     .indexOfLast { it.opcode == Opcode.IGET_OBJECT }
                 
-                val coldHashReg = "v" + (instructions[coldHashIgetObjectIndex] as OneRegisterInstruction).registerA
+                val coldHashReg = implementation!!.instructions.elementAt(coldHashIgetObjectIndex).getRegister()
 
-                // Inject ngay sau khi lấy được chuỗi, để ghi đè nó bằng data của ta trước khi bị check textUtils.isEmpty
-                addInstructions(coldHashIgetObjectIndex + 1,
+                addInstructions(
+                    coldHashIgetObjectIndex + 1,
                     """
                         invoke-static {$coldHashReg}, $EXTENSION_CLASS_DESCRIPTOR->getColdHashData(Ljava/lang/String;)Ljava/lang/String;
                         move-result-object $coldHashReg
-                    """)
+                    """
+                )
 
                 // --- 4. COLD CONFIG GROUP ---
                 val encodeToStringIndex = indexOfFirstInstructionOrThrow(match.stringMatches!![0].index) {
@@ -90,14 +106,15 @@ val freezeLayoutUpdatesPatch = bytecodePatch(
 
                 val coldConfigGroupResultIndex = indexOfFirstInstructionOrThrow(encodeToStringIndex, Opcode.MOVE_RESULT_OBJECT)
                 
-                // Thanh ghi này sẽ tự động bắt được "p1" (được biểu diễn dưới dạng vX theo dexlib2)
-                val coldConfigReg = "v" + (instructions[coldConfigGroupResultIndex] as OneRegisterInstruction).registerA
+                val coldConfigReg = implementation!!.instructions.elementAt(coldConfigGroupResultIndex).getRegister()
 
-                addInstructions(coldConfigGroupResultIndex + 1,
+                addInstructions(
+                    coldConfigGroupResultIndex + 1,
                     """
                         invoke-static {$coldConfigReg}, $EXTENSION_CLASS_DESCRIPTOR->getColdConfigGroup(Ljava/lang/String;)Ljava/lang/String;
                         move-result-object $coldConfigReg
-                    """)
+                    """
+                )
             }
         }
     }
